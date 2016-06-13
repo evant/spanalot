eeed3b84a49783c3936382ea0cb7045388f09d46
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2016-06-13 17:37:32 -0400

    updated build tools, remove holdr from sample

--- sample/src/main/java/me/tatarka/spanalot/sample/MainActivity.java
+++ sample/src/main/java/me/tatarka/spanalot/sample/MainActivity.java
@@ -1,13 +1,11 @@
 package me.tatarka.spanalot.sample;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import me.tatarka.spanalot.Spanalot;
-import me.tatarka.spanalot.sample.holdr.Holdr_ActivityMain;
 
 import static me.tatarka.spanalot.Spanalot.backgroundColor;
 import static me.tatarka.spanalot.Spanalot.style;
@@ -16,19 +14,18 @@ import static me.tatarka.spanalot.Spanalot.textSizeRelative;
 import static me.tatarka.spanalot.Spanalot.underline;
 
 
-public class MainActivity extends ActionBarActivity {
-    Holdr_ActivityMain holdr;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        holdr = new Holdr_ActivityMain(findViewById(android.R.id.content));
+        TextView helloWorld = (TextView) findViewById(R.id.hello_world);
 
         Spanalot spanalot = new Spanalot(backgroundColor(getResources().getColor(R.color.red_200)))
                 .append("Hello, ", style(Typeface.ITALIC))
                 .append("World!", underline(), textColor(getResources().getColor(R.color.purple_900)), textSizeRelative(1.5f));
 
-        holdr.helloWorld.setText(spanalot);
+        helloWorld.setText(spanalot);
     }
 }
