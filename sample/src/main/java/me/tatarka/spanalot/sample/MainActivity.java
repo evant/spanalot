31365118da32766dc3c32d86e4cdd9d748cfad68
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2016-06-13 20:21:09 -0400

    Add new format method.
    This method lets you append to a Spanalot with a format string and some
    args. Unlike String.format(), it preserves spans allowing you to compose
    styled pieces of text easily.

--- sample/src/main/java/me/tatarka/spanalot/sample/MainActivity.java
+++ sample/src/main/java/me/tatarka/spanalot/sample/MainActivity.java
@@ -2,6 +2,7 @@ package me.tatarka.spanalot.sample;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -21,11 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView helloWorld = (TextView) findViewById(R.id.hello_world);
+        TextView helloWorld2 = (TextView) findViewById(R.id.hello_world2);
+        int red = ContextCompat.getColor(this, R.color.red_200);
+        int purple = ContextCompat.getColor(this, R.color.purple_900);
 
-        Spanalot spanalot = new Spanalot(backgroundColor(getResources().getColor(R.color.red_200)))
+        Spanalot spanalot = new Spanalot(backgroundColor(red))
                 .append("Hello, ", style(Typeface.ITALIC))
-                .append("World!", underline(), textColor(getResources().getColor(R.color.purple_900)), textSizeRelative(1.5f));
+                .append("World!", underline(), textColor(purple), textSizeRelative(1.5f));
 
         helloWorld.setText(spanalot);
+
+        Spanalot spanalot2 = new Spanalot(backgroundColor(red))
+                .format("%1$s, %2$s!",
+                        new Spanalot("Hello", style(Typeface.ITALIC)),
+                        new Spanalot("World", underline(), textColor(purple), textSizeRelative(1.5f)));
+
+        helloWorld2.setText(spanalot2);
     }
 }
--- spanalot/src/main/java/me/tatarka/spanalot/Spanalot.java
+++ spanalot/src/main/java/me/tatarka/spanalot/Spanalot.java
@@ -3,6 +3,7 @@ package me.tatarka.spanalot;
 import android.support.annotation.NonNull;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.style.AbsoluteSizeSpan;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
@@ -13,21 +14,33 @@ import android.text.style.SubscriptSpan;
 import android.text.style.SuperscriptSpan;
 import android.text.style.UnderlineSpan;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Formatter;
+import java.util.Locale;
+
 /**
  * A simple wrapper around a spannable to make it easier to work with.
  */
-public class Spanalot implements Spanned {
-    protected SpannableStringBuilder str = new SpannableStringBuilder();
+public final class Spanalot implements Spanned {
+
+    private final SpannableStringBuilder str = new SpannableStringBuilder();
+    private ArrayList<Object> globalSpans;
+    private boolean globalSpansApplied;
+    private FormatRecorder recorder;
 
     /**
      * Constructs a new {@code Spanalot}.
      */
-    public Spanalot() {
+    public Spanalot(Object... spans) {
+        if (spans.length != 0) {
+            globalSpans = new ArrayList<>(spans.length);
+            Collections.addAll(globalSpans, spans);
+        }
     }
 
     /**
-     * Constructs a new {@code Spanalot} with the text and spans.
-     * This is a convenience method for
+     * Constructs a new {@code Spanalot} with the text and spans. This is a convenience method for
      * {@code new Spanalot().append(CharSequence, Object... spans)} for cases where you only have
      * one text segment.
      *
@@ -45,6 +58,7 @@ public class Spanalot implements Spanned {
      * @param spans the spans to apply to the text
      */
     public Spanalot append(CharSequence text, Object... spans) {
+        clearGlobalSpans();
         int start = str.length();
         int end = start + text.length();
         str.append(text);
@@ -54,28 +68,86 @@ public class Spanalot implements Spanned {
         return this;
     }
 
+    /**
+     * Appends a segment of text formatted with the given format string as with {@link
+     * String#format(String, Object...)}. However, all spans are preserved in the format args.
+     *
+     * @param format the format string
+     * @param args   the format args
+     */
+    public Spanalot format(String format, Object... args) {
+        return format(Locale.getDefault(), format, args);
+    }
+
+    /**
+     * Appends a segment of text formatted with the given format string as with {@link
+     * String#format(Locale, String, Object...)}. However, all spans are preserved in the format
+     * args.
+     *
+     * @param format the format string
+     * @param args   the format args
+     */
+    public Spanalot format(Locale locale, String format, Object... args) {
+        if (locale == null) {
+            throw new NullPointerException("locale == null");
+        }
+        if (format == null) {
+            throw new NullPointerException("formatString == null");
+        }
+        clearGlobalSpans();
+        recorder = new FormatRecorder(args);
+        Formatter formatter = new Formatter(recorder, locale);
+        formatter.format(format, recorder.args);
+        recorder.applySpans();
+        recorder = null;
+        return this;
+    }
+
+    private void ensureGlobalSpans() {
+        if (globalSpans != null && !globalSpansApplied) {
+            for (Object span : globalSpans) {
+                str.setSpan(span, 0, str.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+            }
+            globalSpansApplied = true;
+        }
+    }
+
+    private void clearGlobalSpans() {
+        if (globalSpans != null && globalSpansApplied) {
+            for (Object span : globalSpans) {
+                str.removeSpan(span);
+            }
+            globalSpansApplied = false;
+        }
+    }
+
     @Override
     public <T> T[] getSpans(int start, int end, Class<T> type) {
+        ensureGlobalSpans();
         return str.getSpans(start, end, type);
     }
 
     @Override
     public int getSpanStart(Object tag) {
+        ensureGlobalSpans();
         return str.getSpanStart(tag);
     }
 
     @Override
     public int getSpanEnd(Object tag) {
+        ensureGlobalSpans();
         return str.getSpanEnd(tag);
     }
 
     @Override
     public int getSpanFlags(Object tag) {
+        ensureGlobalSpans();
         return str.getSpanFlags(tag);
     }
 
     @Override
     public int nextSpanTransition(int start, int limit, Class type) {
+        ensureGlobalSpans();
         return str.nextSpanTransition(start, limit, type);
     }
 
@@ -91,6 +163,7 @@ public class Spanalot implements Spanned {
 
     @Override
     public CharSequence subSequence(int start, int end) {
+        ensureGlobalSpans();
         return str.subSequence(start, end);
     }
 
@@ -161,5 +234,116 @@ public class Spanalot implements Spanned {
      */
     public static BackgroundColorSpan backgroundColor(int color) {
         return new BackgroundColorSpan(color);
+    }
+
+    /**
+     * Records the locations of formatted strings so spans can be applied.
+     */
+    private class FormatRecorder implements Appendable {
+        Object[] args;
+        int[][] indexes;
+
+        FormatRecorder(Object[] args) {
+            this.args = wrapArgs(args);
+            indexes = new int[args.length][];
+        }
+
+        Object[] wrapArgs(Object[] args) {
+            for (int i = 0; i < args.length; i++) {
+                Object arg = args[i];
+                if (arg instanceof Spanned) {
+                    args[i] = new RecordingSpanned((Spanned) arg);
+                }
+            }
+            return args;
+        }
+
+        void record(int index) {
+            int argIndex = findArgIndex();
+            if (argIndex == -1) {
+                return;
+            }
+            int[] n = indexes[argIndex];
+            if (n == null) {
+                indexes[argIndex] = new int[]{index};
+            } else {
+                int[] newN = new int[n.length + 1];
+                System.arraycopy(n, 0, newN, 0, n.length);
+                newN[newN.length - 1] = index;
+                indexes[argIndex] = newN;
+            }
+        }
+
+        int findArgIndex() {
+            for (int i = 0; i < args.length; i++) {
+                Object arg = args[i];
+                if (arg instanceof RecordingSpanned
+                        && ((RecordingSpanned) arg).takeCalled()) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        void applySpans() {
+            for (int i = 0; i < args.length; i++) {
+                Object arg = args[i];
+                if (arg instanceof RecordingSpanned) {
+                    Spanned spanned = ((RecordingSpanned) arg).str;
+                    int[] n = indexes[i];
+                    for (int start : n) {
+                        TextUtils.copySpansFrom(spanned, 0, spanned.length(), null, str, start);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public Appendable append(char c) {
+            clearGlobalSpans();
+            str.append(c);
+            return this;
+        }
+
+        @Override
+        public Appendable append(CharSequence csq) {
+            if (recorder != null) {
+                recorder.record(str.length());
+            }
+            clearGlobalSpans();
+            str.append(csq);
+            return this;
+        }
+
+        @Override
+        public Appendable append(CharSequence csq, int start, int end) {
+            if (recorder != null) {
+                recorder.record(str.length());
+            }
+            clearGlobalSpans();
+            str.append(csq, start, end);
+            return this;
+        }
+    }
+
+    private class RecordingSpanned {
+        Spanned str;
+        boolean called;
+
+        RecordingSpanned(Spanned str) {
+            this.str = str;
+        }
+
+        @Override
+        public String toString() {
+            called = true;
+            return str.toString();
+        }
+
+        boolean takeCalled() {
+            boolean result = called;
+            called = false;
+            return result;
+        }
     }
 }
